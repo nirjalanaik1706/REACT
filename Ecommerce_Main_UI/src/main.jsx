288d7e5870500f392dbacc1aa8feff28fe16c15b
NIRJALA DATTATRAY NAIK <158537275+nirjalanaik1706@users.noreply.github.com>
2026-03-18 18:10:43 +0530

    Uncomment BrowserRouter import in main.jsx

--- Ecommerce_Main_UI/src/main.jsx
+++ Ecommerce_Main_UI/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
