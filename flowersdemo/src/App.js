8928440390f2eafa5166fdd0be417c0c292ab0b9
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-03-31 16:00:21 +0530

    props and states understanding

--- flowersdemo/src/App.js
+++ flowersdemo/src/App.js
@@ -1,26 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';  
+import List from './List';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+        <BasicRouting />
+      </div>
+    </Router>
+  );
+}
+
+function BasicRouting() {
+  return (
+    <div className='text-center'>
+      <h1>Transflower Store</h1>
+
+      <Link to="/Home">Home</Link> | 
+      <Link to="/About">About us</Link> | 
+      <Link to="/Contact">Contact</Link> | 
+      <Link to="/Flowers">Flowers</Link> | 
+      <Link to="/Login">Login</Link> | 
+      <Link to="/Register">Register</Link>
+
+      <hr />
+
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Flowers" element={<List />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
 
+function Home() {
+  return (
+    <>
+      <div className='jumbotron text-center'>
+        <h1>Welcome to Transflower Learning Pvt. Ltd.</h1>
+        <p>Mentor as a service</p>
+        <p>React js learning is fun</p>
+      </div>
+
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-4'>
+            <p>Flowers</p>
+            <p>Celebrate every moment with flowers</p>
+          </div>
+
+          <div className='col-sm-4'>
+            <p>Gifts</p>
+            <p>Find the perfect gift for every occasion</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About us</h2>
+      <h3>Transflower Agro Service</h3>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contact us</h2>
+      <p>Contact us at: +91 1234567890</p>
+    </div>
+  );
+}
 
 export default App;
---
+++ flowersdemo/src/Product.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./App.css";
+import Counter from "./Counter";
+
+class Product extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state={likes:props.likes};
+        this.handler=this.handler.bind(this);
+    }
+    handler(data){
+        this.setState({likes:data});
+
+    }
+    render() {
+        return (
+            <div className="text-center">
+                <h2>Flower Details</h2>
+                    <p><b>Title:</b> {this.props.title}</p>
+                    <img src={this.props.imageurl} alt="flower"/>
+                    <p><b>Description:</b> {this.props.description}</p>
+                    <p><b>Quantity available:</b> {this.props.quantity}</p>
+                    <p><b>Unit Price:</b> {this.props.unitprice}</p>
+                    <p><b>Total likes:</b> {this.state.likes}</p>
+                    <Counter count={this.props.likes} handler={this.handler}> </Counter>
+                    <br />
+                    <button>Add to cart</button>
+            </div>
+        );
+    }
+}
+
+export default Product;
